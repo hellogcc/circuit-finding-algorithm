2a45569f60c4e54ac921a6d5110cc7e03f42276f
Ankit Kumar <ankitkumar.itbhu@gmail.com>
2017-07-26 11:40:01 +0530

    changed example graph

--- CircuitFinder.h
+++ CircuitFinder.h
@@ -1,14 +1,3 @@
-//===----------------------------------------------------------------------===//
-//
-// Written by Xing Mingjie (mingjie.xing@gmail.com).
-//
-// An implementation of the Johnson's circuit finding algorithm [1].
-//
-// [1] Donald B. Johnson, Finding all the elementary circuits of a directed
-//     graph, SIAM Journal on Computing, 1975.
-//
-//===----------------------------------------------------------------------===//
-
 #ifndef CIRCUIT_FINDER_H
 #define CIRCUIT_FINDER_H
 
